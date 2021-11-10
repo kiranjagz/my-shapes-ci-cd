26ac48c739373c557db6555206284f7af4bbf233
Kiran Jagathlal <kiranjagz@gmail.com>
2021-11-10 21:48:40 +0200

    Made some code and yml changes.

--- Shapes.Api/Controllers/Ping.cs
+++ Shapes.Api/Controllers/Ping.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Does.Stuff.Multiple;
+using Does.Stuff.With.Logic.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Shapes.Api.Services;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,18 +24,28 @@ namespace Shapes.Api.Controllers
         [HttpGet]
         public IActionResult Get()
         {
+            var numberOne = 30; var numberTwo = 3;
+            _logger.LogInformation($"Number One used was: {numberOne} and number two was: {numberTwo}");
+
             var message = _pingy.Ping();
-            var subtraction = new Does.Stuff.With.Logic.Services.Subtraction();
-            var result = subtraction.SubtractTwoNumbers(10, 3);
-            var multiple = new Does.Stuff.Multiple.Multiple();
-            var multipleResult = multiple.MultipleTwoNumbers(10, 3);
 
-            _logger
-                .Log(LogLevel.Information, $"Ping controller called with message: {result} and multiple: {multipleResult}");
-            _logger
-                .Log(LogLevel.Information, "Somehow I managed to get the nuget package building with this project, ha!");
+            var subtraction = new Subtraction();
+            var subtractionResult = subtraction.SubtractTwoNumbers(numberOne, numberTwo);
+            _logger.LogInformation($"The subtraction result was: {subtractionResult}");
 
-            return Ok($"{message} :: subraction {result} and multiple :: {multipleResult}");
+            var multiple = new Multiple();
+            var multipleResult = multiple.MultipleTwoNumbers(numberOne, numberTwo);
+            _logger.LogInformation($"The multiple result was: {multipleResult}");
+
+            _logger.Log(LogLevel.Information, "Somehow I managed to get the nuget package building with this project, ha!");
+
+            return Ok(new
+            {
+                message = message,
+                subtractionResult = subtractionResult,
+                multipleResult = multipleResult,
+                dateTime = DateTime.Now.ToString()
+            });
         }
     }
 }
