aff26ddd6e2cbe4b294c3f79f8281478b97575be
Kiran <kiranjagz@gmai.com>
2022-01-24 22:06:07 +0200

    Use secrets.json

--- src/Shapes.Api/Models/AppSettings.cs
+++ src/Shapes.Api/Models/AppSettings.cs
@@ -3,6 +3,7 @@
     public class AppSettings
     {
         public string Secret { get; set; }
+        public string ApiKey { get; set; }  
         public string Value1 { get; set; }
         public string Name { get; set; }
 
